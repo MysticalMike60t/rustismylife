47a437c65fad940260c4948ea45c62504b290c9d
Butter <121118774+MysticalMike60t@users.noreply.github.com>
2026-01-25 00:29:07 -0700

    Meow

---
+++ generate_favicon.sh
@@ -0,0 +1,5 @@
+# TODO: Make this script work for any use case (interactive/cmdline)
+
+echo "NOT FUNCTIONAL YET"
+
+# magick "bo_o_dg_logo@2x.png" -gravity center -define icon:auto-resize="256,128,96,64,48,32,16" -colors 256 -background transparent ../../../public/favicon.ico
